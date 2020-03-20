23c911ede6177a993108f85a333931455165ca90
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-03-20 02:19:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -370,7 +370,8 @@ def big_shoe_rebounds
   all_players.each_pair do |name, stats|
     stats.each_pair do |player_key, player_value|
       
-      if player_key == 
+      if player_key == :shoe
+        if player_value > shoe_size
       
     end
     
